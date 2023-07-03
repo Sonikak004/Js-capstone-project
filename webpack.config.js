7d639b37bb46ff3497811811638500ef6a027d68
Alfred Peter <alpeter91@gmail.com>
2023-07-03 16:52:34 +0300

    Add project dependencies

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/template.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      // stylesheets
+      {
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+      },
+      // images
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+      },
+      // font
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
