bbba4c03747b41cc1bec5e004017ef6709ab51dd
Alfred Peter <alpeter91@gmail.com>
2023-07-05 07:24:00 +0300

    Fetch weight and height from api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+
+const btn = document.getElementById('poke');
+
+btn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const poke = e.target.name;
+  console.log(poke);
+  const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`);
+  const data = await result.json();
+  const weight = data.weight
+  console.log(weight)
+  const height = data.height
+  console.log(height)
+  const abilities = []
+  const moves = []
+})
