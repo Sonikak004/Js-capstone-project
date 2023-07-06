10963c528797361563dcedb6195ba7676ac2c93c
sonika <sonikak004@gmail.com>
2023-07-06 11:35:13 +0100

    Add likes div to the popup

--- src/popupTemplate.js
+++ src/popupTemplate.js
@@ -1,12 +1,11 @@
-const template = (data, abilities, moves) => `
-<div id="popup">
-  <a href="#" id="closePop">
-        <i class="fas fa-times fa-lg"></i></a>
+const template = (data, abilities, moves, likesCount) => `
+  <div id="popup">
+    <a href="#" id="closePop">
+      <i class="fas fa-times fa-lg"></i>
+    </a>
     <figure id="popPic">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-  data.id
-}.svg" alt="${data.name}">
-        <figcaption><h1>${data.name}</h1></figcaption>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg" alt="${data.name}">
+      <figcaption><h1>${data.name}</h1></figcaption>
     </figure>
     <div class="likes-container">
       <button class="like-button">
@@ -15,19 +14,20 @@ const template = (data, abilities, moves) => `
       <span id="likesCount">${likesCount}</span>
     </div>
     <div class="details">
-        <div class="stats">Height: ${data.height}</div>
-        <div class="stats">Weight: ${data.weight}</div>
-        <div class="stats">Abilities:
-            <ul>
-            ${abilities.map((ability) => `<li>${ability}</li>`).join('')}
-            </ul>
-        </div>
-        <div class="stats">Moves:
-            <ul>
-            ${moves.map((move) => `<li>${move}</li>`).join('')}
-            </ul>
-        </div>  
+      <div class="stats">Height: ${data.height}</div>
+      <div class="stats">Weight: ${data.weight}</div>
+      <div class="stats">Abilities:
+        <ul>
+          ${abilities.map((ability) => `<li>${ability}</li>`).join('')}
+        </ul>
+      </div>
+      <div class="stats">Moves:
+        <ul>
+          ${moves.map((move) => `<li>${move}</li>`).join('')}
+        </ul>
+      </div>  
     </div>
+  </div>
 `;
 
 export default template;
