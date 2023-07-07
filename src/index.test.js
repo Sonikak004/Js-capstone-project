bc60848da435070479aefdb45f3b331f82921aad
sonika <sonikak004@gmail.com>
2023-07-07 09:43:02 +0100

    Add dummy mockup of html element to test

---
+++ src/index.test.js
@@ -0,0 +1,15 @@
+    document.body.innerHTML = `
+        <div id="poke-list">
+          <div class="poke-grid">
+            <div class="item">
+              <h2>Pokemon 1</h2>
+            </div>
+            <div class="item">
+              <h2>Pokemon 2</h2>
+            </div>
+          </div>
+        </div>
+        <div id="counter"></div>
+      `;
+  });
+
